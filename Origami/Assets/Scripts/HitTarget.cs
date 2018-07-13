13cfe8a28b1effdd9d7ef0433bf05b5f16f027dd
RobinGoos <goosrobin1@gmail.com>
2018-07-13 13:08:48 +0100

    tutorial project

---
+++ Origami/Assets/Scripts/HitTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTarget : MonoBehaviour {
+
+    public GameObject underworld;
+    public GameObject objectToHide;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        objectToHide.SetActive(false);
+        underworld.SetActive(true);
+
+        SpatialMapping.Instance.MappingEnabled = false;
+    }
+}
---
+++ Origami/Assets/Scripts/TapToPlaceParent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapToPlaceParent : MonoBehaviour {
+    bool placing = false;
+
+    void OnSelect()
+    {
+        placing = !placing;
+
+        if (placing)
+        {
+            SpatialMapping.Instance.DrawVisualMeshes = true;
+        }
+        else
+        {
+            SpatialMapping.Instance.DrawVisualMeshes = false;
+        }
+    }
+
+    void Update()
+    {
+        if (placing)
+        {
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit hitInfo;
+            if(Physics.Raycast(headPosition, gazeDirection, out hitInfo, 30.0f, SpatialMapping.PhysicsRaycastMask))
+            {
+                this.transform.parent.position = hitInfo.point;
+
+                Quaternion toQuat = Camera.main.transform.localRotation;
+                toQuat.x = 0;
+                toQuat.y = 0;
+                this.transform.parent.rotation = toQuat;
+            }
+        }   
+    }
+}
